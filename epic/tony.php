599c854d59c82f7f4a67090817ff1ad7be710326
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-11 17:58:23 -0600

    Added name, gender, age, marital status and income.  Also added image.

--- epic/tony.php
+++ epic/tony.php
@@ -1,7 +1,27 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ryanbecker
- * Date: 7/11/18
- * Time: 4:42 PM
- */
+<!DOCTYPE html>
+<html lang="en"></html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Tony Raines</title>
+	</head>
+<body>
+	<h1>Personal Info</h1>
+	<ul>
+		<li>Name: Tony Raines</li>
+		<li>Gender: Male</li>
+		<li>Age: 23</li>
+		<li>Marital Status: Single</li>
+		<li>Profession: Fedex Delivery Driver</li>
+		<li>Income: $35k a year</li>
+	</ul>
+	<img src="/epic/tony.png" alt="image of tony" style="width: 350px;height:480px;>
+
+
+
+
+
+
+
+
+</body>
+</html>
