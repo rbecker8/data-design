aedfbe15ff7498f4c88190daf553559fa8c202cf
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-11 22:00:31 -0600

    Added bullet to use case.

--- epic/use-case.php
+++ epic/use-case.php
@@ -9,11 +9,11 @@
 				<p>Tony needs to find games reviews to make educated purchases</p>
 			<h2>Use Case</h2>
 				<ul>
-				<li>Tony navigates to his favorite video website</li>
-				<li>Site displays reviews sorted by game console</li>
+				<li>Tony navigates to his favorite video game review website</li>
+				<li>Site displays reviews sorted by game console as well as news</li>
 				<li>Tony clicks Xbox tab</li>
 				<li>Site displays a list of game reviews sorted by most recent</li>
-				<li>Tony clicks a game that all his friends have been talking about</li>
+				<li>Tony clicks game that all his friends have been talking about</li>
 				<li>Site displays game review including 0-10 score and user comments</li>
 				<li>Tony enjoys reading game review</li>
 				</ul>
