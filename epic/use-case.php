9b1b8a786df2218993e994fa81c2a62add0e66ce
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-12 08:56:50 -0600

    Changed & to and.

--- epic/use-case.php
+++ epic/use-case.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>User Story & Use Case</title>
+		<title>User Story and Use Case</title>
 		<link rel="stylesheet" href="./styles/styles.css">
 	</head>
 		<body>
