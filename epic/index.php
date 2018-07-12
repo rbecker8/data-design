91a5c85c3df84df4e45c558724d592ce3b41cfda
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-12 08:01:04 -0600

    Changed font colors to black.  Added style to index page.

--- epic/index.php
+++ epic/index.php
@@ -4,9 +4,9 @@
 		<meta charset="UTF-8">
 		<title>Navigation</title>
 	</head>
-	<h1>Data Design</h1>
+	<h1 class="main-title">Data Design</h1>
 		<body>
-		<ul>
+		<ul class="center-list">
 			<li><a href="tony.php">Persona</a> </li>
 			<li><a href="use-case.php">User Story & Use Case</a> </li>
 			<li><a href="conceptual-model.php">Conceptual Model</a> </li>
