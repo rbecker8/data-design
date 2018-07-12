ca77683e41b99581c45adc4c9843f991f4113152
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-12 09:04:24 -0600

    Amended indentation on conceptual model page.  Also added relations section with content.

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -7,16 +7,21 @@
 		<body>
 		<h1 class="main-title">Conceptual Model</h1>
 		<p class="main-title">Game</p>
-		<ul class="center-list">
-			<li>gameId (Primary Key)</li>
-			<li>gameConsoleId</li>
-			<li>gameReleaseId</li>
-		</ul>
+			<ul class="center-list">
+				<li>gameId (Primary Key)</li>
+				<li>gameConsoleId</li>
+				<li>gameReleaseId</li>
+			</ul>
 		<p class="main-title">Game Reviewer</p>
-		<ul class="center-list">
-			<li>reviewerId (Primary Key)</li>
-			<li>reviewDateId</li>
-		</ul>
+			<ul class="center-list">
+				<li>reviewerId (Primary Key)</li>
+				<li>reviewDateId</li>
+			</ul>
+		<p>Relations</p>
+			<ul>
+				<li>One reviewer can review multiple games (1 to n)</li>
+				<li>One game is reviewed by one reviewer (1 to 1)</li>
+			</ul>
 		<p class="main-title"><a href="index.php">Home</a></p>
 		</body>
 </html>
