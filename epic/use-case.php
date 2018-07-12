b4bb352446a88faec756654b26e55809d7c66284
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-11 21:59:18 -0600

    Added user story with content. Added use case with content.

--- epic/use-case.php
+++ epic/use-case.php
@@ -4,22 +4,18 @@
 		<meta charset="UTF-8">
 		<title>Tony Raines</title>
 	</head>
-	<body>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	</body>
+		<body>
+			<h1>User Story</h1>
+				<p>Tony needs to find games reviews to make educated purchases</p>
+			<h2>Use Case</h2>
+				<ul>
+				<li>Tony navigates to his favorite video website</li>
+				<li>Site displays reviews sorted by game console</li>
+				<li>Tony clicks Xbox tab</li>
+				<li>Site displays a list of game reviews sorted by most recent</li>
+				<li>Tony clicks a game that all his friends have been talking about</li>
+				<li>Site displays game review including 0-10 score and user comments</li>
+				<li>Tony enjoys reading game review</li>
+				</ul>
+		</body>
 </html>
