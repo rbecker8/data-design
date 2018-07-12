5e41fa21dfc5741a8212e6ebc90d613c3ac142fe
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-11 20:28:48 -0600

    changed path to image.  Added paragraph and list of attitude.

--- epic/index.php
+++ epic/index.php
@@ -1,7 +1,8 @@
-<?php
+<!DOCTYPE HTML>
 /**
  * Created by PhpStorm.
  * User: ryanbecker
  * Date: 7/11/18
  * Time: 4:44 PM
  */
+<img src="tony.png" alt="image of tony" style="width: 350px;height:480px">
--- epic/tony.php
+++ epic/tony.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="en"></html>
+<html lang="en">
 	<head>
 		<meta charset="UTF-8">
 		<title>Tony Raines</title>
 	</head>
 <body>
 	<h1>Personal Info</h1>
+	<p><img src="tony.png" alt="image of tony"></p>
 	<ul>
 		<li>Name: Tony Raines</li>
 		<li>Gender: Male</li>
@@ -14,7 +15,15 @@
 		<li>Profession: Fedex Delivery Driver</li>
 		<li>Income: $35k a year</li>
 	</ul>
-	<img src="tony.png" alt="image of tony" style="width: 350px;height:480px;>
+	<br>
+	<p>Attitude</p>
+	<ul>
+		<li>Tony enjoys reading and researching online</li>
+		<li>Especially when it comes to new video games</li>
+		<li>However he doesn't like having to search for content</li>
+		<li>He doesn't like websites that fail to organize games into specific lists like console type or genre</li>
+	</ul>
+
 
 
 
