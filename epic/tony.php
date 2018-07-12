af802518ea235cfaf502d012d23c70a7a40959bc
Ryan Becker <rbecker8@cnm.edugit config --global user.name Ryan>
2018-07-11 21:05:36 -0600

    Added content to attitude, likes and needs sections.

--- epic/tony.php
+++ epic/tony.php
@@ -16,13 +16,24 @@
 		<li>Income: $35k a year</li>
 	</ul>
 	<p>Attitude</p>
-	<ul>
-		<li>Tony enjoys reading and researching online</li>
-		<li>Especially when it comes to new video games</li>
-		<li>However he doesn't like having to search for content</li>
-		<li>He doesn't like websites that fail to organize games into specific lists like console type or genre</li>
-	</ul>
-<	<p></p>
+		<ul>
+			<li>Tony enjoys reading and researching online</li>
+			<li>Especially when it comes to new video games</li>
+			<li>However he doesn't like having to search for content</li>
+			<li>He doesn't like websites that fail to organize games into specific lists like console type or genre</li>
+		</ul>
+	<p>Likes</p>
+		<ul>
+			<li>Likes driving sim games</li>
+			<li>Likes first person shooters</li>
+			<li>Likes web content that is free to view and easy to access</li>
+		</ul>
+	<p>Needs</p>
+		<ul>
+		<li>Websites optimized for mobile browsing as his always driving and rarely sitting a desk</li>
+		<li>Honest game critiques that go further than a subjective review</li>
+		<li>A 0-10 ranking would be ideal</li>
+		</ul>
 
 
 
