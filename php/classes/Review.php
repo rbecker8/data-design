8a68ea7dbe81e494c9846b9feb34decfb5f2e846
rbecker8 <rbecker8@cnm.edu>
2018-07-19 13:56:33 -0600

    Added accessor & mutator method for review rating & review content.

--- php/classes/Review.php
+++ php/classes/Review.php
@@ -18,7 +18,7 @@ class Review {
 	private $reviewId;
 	/**
 	 * id of the Reviewer that wrote this Review; this is a foreign key
-	 *@var Uuid  $reviewReviewerId
+	 * @var Uuid $reviewReviewerId
 	 **/
 	private $reviewReviewerId;
 	/**
@@ -28,7 +28,7 @@ class Review {
 	private $reviewConsole;
 	/**
 	 * date of review release
-	 * @var DateTime $reviewReleaseDate*
+	 * @var DateTime $reviewReleaseDate *
 	 **/
 	private $reviewDate;
 	/**
@@ -48,8 +48,8 @@ class Review {
 	 *
 	 * @return Uuid value for review id
 	 **/
-	public function getReviewId() : Uuid {
-		return($this->reviewId);
+	public function getReviewId(): Uuid {
+		return ($this->reviewId);
 	}
 
 
@@ -60,7 +60,7 @@ class Review {
 	 * @throws \RangeException if $newReviewId is n
 	 * @throws \TypeError if $newTweetId is not a uuid.e
 	 **/
-	public function setReviewId( $newReviewId) : void {
+	public function setReviewId($newReviewId): void {
 		try {
 			$uuid = self::validateUuid($newReviewId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
@@ -78,8 +78,8 @@ class Review {
 	 *
 	 * @return Uuid value of review reviewer id
 	 */
-	public function getReviewReviwerId() : Uuid{
-		return($this->reviewReviewerId);
+	public function getReviewReviewerId(): Uuid {
+		return ($this->reviewReviewerId);
 	}
 
 	/**
@@ -89,7 +89,7 @@ class Review {
 	 * @throws \RangeException if $newReviewerId is not positive
 	 * @throws \TypeError if $newReviewReviewerId is not a UUI
 	 */
-	public function setReviewReviewerId( $newReviewReviewerId) : void {
+	public function setReviewReviewerId($newReviewReviewerId): void {
 		try {
 			$uuid = self::validateUuid($newReviewReviewerId);
 		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
@@ -100,13 +100,14 @@ class Review {
 		// convert and store reviewer id
 		$this->reviewReviewerId = $uuid;
 	}
+
 	/**
 	 * accessor method for review console
 	 *
 	 * @return string value of review console
 	 */
-	public function getReviewConsole() : string {
-		return($this->reviewConsole);
+	public function getReviewConsole(): string {
+		return ($this->reviewConsole);
 	}
 
 	/**
@@ -114,10 +115,10 @@ class Review {
 	 *
 	 * @param string $newReviewConsole new value of review console
 	 * @throws \InvalidArgumentException if $newReviewConsole is not a string or insecure
-	 * @throws \RangeException if $newTweetContent is > 16 characters
-	 * @throws \TypeError if $newTweetContent is not a string
+	 * @throws \RangeException if $newReviewConsole is > 16 characters
+	 * @throws \TypeError if $newReviewConsole is not a string
 	 **/
-	public function setReviewConsole(string $newReviewConsole) : void {
+	public function setReviewConsole(string $newReviewConsole): void {
 		// verify the review console is secure
 		$newReviewConsole = trim($newReviewConsole);
 		$newReviewConsole = filter_var($newReviewConsole, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
@@ -125,7 +126,7 @@ class Review {
 			throw(new \InvalidArgumentException("review console is empty or insecure"));
 		}
 
-		//verify the tweet content will fit in the database
+		//verify the review console will fit in the database
 		if(strlen($newReviewConsole) > 16) {
 			throw(new \RangeException("review console too large"));
 		}
@@ -133,13 +134,14 @@ class Review {
 		//store the review console
 		$this->reviewConsole = $newReviewConsole;
 	}
+
 	/**
 	 * accessor method for review date
 	 *
 	 * @return \DateTime value of review date
 	 **/
-	public function getReviewDate() : \DateTime {
-		return($this->reviewDate);
+	public function getReviewDate(): \DateTime {
+		return ($this->reviewDate);
 	}
 
 	/**
@@ -149,7 +151,7 @@ class Review {
 	 * @throws \InvalidArgumentException if $newReviewDate is not a valid object or string
 	 * @throws \RangeException if $newReviewDate is a date that does not exist
 	 **/
-	public function setReviewDate($newReviewDate = null) : void {
+	public function setReviewDate($newReviewDate = null): void {
 		//base case: if the date is null, use current date and time
 		if($newReviewDate === null) {
 			$this->reviewDate = new \DateTime();
@@ -164,15 +166,69 @@ class Review {
 			throw(new $exceptionType($exception->getMessage(), 0, $exception));
 		}
 		$this->reviewDate = $newReviewDate;
-}
-
-
-
-
-
-
-
-
-
-
 	}
+
+	/**
+	 * accessor method for review rating
+	 *
+	 * @return int value of review rating
+	 **/
+	public function getReviewRating(): int {
+		return ($this->reviewRating);
+	}
+
+	/**
+	 * mutator method for review rating
+	 *
+	 * @param int|null $newReviewRating new value of review rating
+	 * @throws \InvalidArgumentException if $newReviewRating is not an int or insecure
+	 * @throws \RangeException if $newReviewRating is > 2 int
+	 * @throws \TypeError if $newReviewRating is not an int
+	 **/
+	public function setReviewRating($newReviewRating): void {
+		// verify the review rating is secure
+		if($newReviewRating < 0) {
+			throw new \RangeException("rating is empty or insecure");
+		}
+
+		//verify the review rating will fit in the database
+		if(($newReviewRating) > 2) {
+			throw(new \RangeException("review rating too large"));
+		}
+		// store review rating
+		$this->reviewRating = $newReviewRating;
+	}
+
+	/**
+	 * accessor method for review content
+	 *
+	 * @return string value of review content
+	 **/
+	public function getReviewContent() : string {
+		return($this->reviewContent);
+	}
+
+	/**
+	 * mutator method for review content
+	 *
+	 * @param string $newReviewContent new value of review content
+	 * @throws \InvalidArgumentException if $newReviewContent is not a string or insecure
+	 * @throws \RangeException if $newTweetContent is > 40000 characters
+	 * @throws \TypeError if $newReviewContent is not a string
+	 */
+	public function setReviewContent(string $newReviewContent) : void {
+		// verify the review content is secure
+		$newReviewContent = trim($newReviewContent);
+		$newReviewContent = filter_var($newReviewContent, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newReviewContent) === true) {
+			throw(new \InvalidArgumentException("review content is empty or insecure"));
+		}
+
+		// verify the review content will fit in the database
+		if(strlen($newReviewContent) > 40000) {
+			throw(new \RangeException("review content too large"));
+		}
+		// store the review content
+		$this->reviewContent = $newReviewContent;
+	}
+}
